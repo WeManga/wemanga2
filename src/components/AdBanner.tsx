257632d8851a312e2c5b70c7d7ace37916d4cd26
WeManga <contact.wemanga@gmail.com>
2025-08-10 19:20:01 +0200

    Update AdBanner.tsx

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -6,16 +6,16 @@ interface AdBannerProps {
 }
 
 const urls: { [key: string]: string } = {
-  homeTop: 'https://www.profitableratecpm.com/w10hh3jj?key=c6bcdb41a9c6df1077cf7048072493e5',
-  homeClassics: 'https://www.profitableratecpm.com/q5gvxneh?key=0fcb2820527b80ee08f776218cee82d9',
-  homeCatalog: 'https://www.profitableratecpm.com/f5dcmeijf?key=ae05b8e83b116293e4e9dfcae2b98c14',
-  homeBottom: 'https://www.profitableratecpm.com/jd3u29qy?key=a77bac4003cf2f4a2e675dfc206fff89',
-  seriesTop: 'https://www.profitableratecpm.com/h33ns6axj?key=79c42dfb3c0d602f602329dc68c40c12',
-  seriesBottom: 'https://www.profitableratecpm.com/v5503e0p?key=e1681898885ce1a63a9df8c24fb3b047',
-  filmsTop: 'https://www.profitableratecpm.com/qeith0eve?key=645019c901b69978a1618a62f5ed5571',
-  filmsBottom: 'https://www.profitableratecpm.com/v269kae51?key=1f3e4fb205f3473733b252968b96bd21',
-  playerTop: 'https://www.profitableratecpm.com/hi3izs3u5u?key=29fe0c6ef8a0941343562387a22b9bc4',
-  playerBottom: 'https://www.profitableratecpm.com/bkn63wpz?key=74d6b49d2b10bfdc736f8e970982c4c4',
+  homeTop: 'https://otieu.com/4/9691962',
+  homeClassics: 'https://otieu.com/4/9691962',
+  homeCatalog: 'https://otieu.com/4/9691962',
+  homeBottom: 'https://otieu.com/4/9691962',
+  seriesTop: 'https://otieu.com/4/9692384',
+  seriesBottom: 'https://otieu.com/4/9692384',
+  filmsTop: 'https://otieu.com/4/9692384',
+  filmsBottom: 'https://otieu.com/4/9692384',
+  playerTop: 'https://otieu.com/4/9692384',
+  playerBottom: 'https://otieu.com/4/9692384',
 };
 
 const AdBanner: React.FC<AdBannerProps> = ({ id, className }) => {
